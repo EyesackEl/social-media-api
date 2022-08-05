efc7584ab0ae135ae77d9ee11ba7a27b231f1446
Eyesack <isaac.laflamme@gmail.com>
2022-08-05 14:56:26 -0700

    controller skeleton completed

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports =  {
+    getUser(req, res) {
+
+    },
+    getUsers(req, res) {
+
+    },
+    newUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    removeFriend(req, res) {
+
+    },
+}
